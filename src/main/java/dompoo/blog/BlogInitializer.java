6e0e3f2b78eac0ce3f1cf8ef2d54114cb9280c83
Dompoo <dlckdrms0517@gmail.com>
2023-09-06 14:35:02 +0900

    feat : 더미데이터 내용 변경

--- src/main/java/dompoo/blog/BlogInitializer.java
+++ src/main/java/dompoo/blog/BlogInitializer.java
@@ -28,19 +28,39 @@ public class BlogInitializer {
 
     @PostConstruct
     public void init() {
-        MemberResponseDto memberDto1 = memberService.join(new MemberSaveDto("username1", "password1"));
-        MemberResponseDto memberDto2 = memberService.join(new MemberSaveDto("username2", "password2"));
+        MemberResponseDto memberDto1 = memberService.join(new MemberSaveDto("윤동주", "password1"));
+        MemberResponseDto memberDto2 = memberService.join(new MemberSaveDto("나태주", "password2"));
 
-        WritingResponseDto writingDto1 = writingService.saveWrite(new WritingSaveDto("title1", "writingContent1", memberDto1.getId()));
-        WritingResponseDto writingDto2 = writingService.saveWrite(new WritingSaveDto("title2", "writingContent2", memberDto1.getId()));
-        WritingResponseDto writingDto3 = writingService.saveWrite(new WritingSaveDto("title3", "writingContent3", memberDto1.getId()));
-        WritingResponseDto writingDto4 = writingService.saveWrite(new WritingSaveDto("title4", "writingContent4", memberDto2.getId()));
+        WritingResponseDto writingDto1 = writingService.saveWrite(new WritingSaveDto(
+                "서시",
+                "죽는 날까지 하늘을 우러러\n" +
+                        "한 점 부끄럼이 없기를,\n" +
+                        "잎새에 이는 바람에도 \n" +
+                        "나는 괴로워했다.\n" +
+                        "별을 노래하는 마음으로 \n" +
+                        "모든 죽어가는 것을 사랑해야지\n" +
+                        "그리고 나한테 주어진 길을 \n" +
+                        "걸어가야겠다.\n\n" +
+                        "오늘 밤에도 별이 바람에 스치운다.",
+                memberDto1.getId()));
+        WritingResponseDto writingDto2 = writingService.saveWrite(new WritingSaveDto(
+                "내가 좋아하는 사람",
+                "내가 좋아하는 사람은\n" +
+                        "슬퍼할 일을 마땅히 슬퍼하고\n" +
+                        "괴로워할 일을 마땅히\n" +
+                        "괴로워하는 사람\n\n" +
+                        "남의 앞에 섰을 때\n" +
+                        "교만하지 않고\n" +
+                        "남의 뒤에 섰을 때\n" +
+                        "비굴하지 않은 사람\n" +
+                        "\n" +
+                        "내가 좋아하는 사람은\n" +
+                        "미워할 것을 마땅히 미워하고\n" +
+                        "사랑할 것을 마땅히 사랑하는\n" +
+                        "그저 보통의 사람",
+                memberDto2.getId()));
 
-        commentService.saveComment(new CommentSaveDto("commentContent", writingDto1.getId(), memberDto1.getId()));
-        commentService.saveComment(new CommentSaveDto("commentContent", writingDto1.getId(), memberDto1.getId()));
-        commentService.saveComment(new CommentSaveDto("commentContent", writingDto1.getId(), memberDto2.getId()));
-        commentService.saveComment(new CommentSaveDto("commentContent", writingDto2.getId(), memberDto1.getId()));
-        commentService.saveComment(new CommentSaveDto("commentContent", writingDto3.getId(), memberDto2.getId()));
-        commentService.saveComment(new CommentSaveDto("commentContent", writingDto4.getId(), memberDto2.getId()));
+        commentService.saveComment(new CommentSaveDto("님 짱이네요.", writingDto1.getId(), memberDto2.getId()));
+        commentService.saveComment(new CommentSaveDto("님 넘 멋져요.", writingDto2.getId(), memberDto1.getId()));
     }
 }
