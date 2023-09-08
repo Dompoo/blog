4ff719d97f6081582a63da9d1e5509ce918f93cd
Dompoo <dlckdrms0517@gmail.com>
2023-09-08 17:06:33 +0900

    feat : 글 수정 기능 추가

--- src/main/java/dompoo/blog/writing/WritingController.java
+++ src/main/java/dompoo/blog/writing/WritingController.java
@@ -3,17 +3,21 @@ package dompoo.blog.writing;
 import dompoo.blog.member.MemberService;
 import dompoo.blog.writing.dto.WritingResponseDto;
 import dompoo.blog.writing.dto.WritingSaveDto;
+import dompoo.blog.writing.dto.WritingUpdateDto;
 import dompoo.blog.writing.form.WritingCreateForm;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.security.Principal;
 import java.util.ArrayList;
@@ -22,6 +26,7 @@ import java.util.List;
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/writing")
+@Slf4j
 public class WritingController {
 
     private final WritingService writingService;
@@ -67,5 +72,33 @@ public class WritingController {
         ));
 
         return "redirect:/writing";
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/modify/{id}")
+    public String modify(WritingCreateForm writingCreateForm, @PathVariable("id") Long writingId, Principal principal) {
+        WritingResponseDto writing = writingService.findOne(writingId);
+        if (!writing.getUsername().equals(principal.getName())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "수정권한이 없습니다.");
+        }
+        log.info("{} {}", writing.getTitle(), writing.getContent());
+        writingCreateForm.setTitle(writing.getTitle());
+        writingCreateForm.setContent(writing.getContent());
+        return "writing_form";
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @PostMapping("/modify/{id}")
+    public String modifyWriting(@Valid WritingCreateForm writingCreateForm, BindingResult bindingResult, @PathVariable("id") Long writingId, Principal principal) {
+        if (bindingResult.hasErrors()) {
+            return "writing_form";
+        }
+        WritingResponseDto writing = writingService.findOne(writingId);
+        if (!writing.getUsername().equals(principal.getName())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "수정권한이 없습니다.");
+        }
+        writingService.updateWriting(new WritingUpdateDto(writingId, writingCreateForm.getTitle(), writingCreateForm.getContent()));
+
+        return String.format("redirect:/writing/%s", writingId);
     }
 }
