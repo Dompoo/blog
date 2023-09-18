23098bb9da159c0a5a84bef258e1981137921257
Dompoo <dlckdrms0517@gmail.com>
2023-09-18 14:21:25 +0900

    feat : 대댓글 MVC 클래스 작성

---
+++ src/main/java/dompoo/blog/reply/ReplyController.java
@@ -0,0 +1,11 @@
+package dompoo.blog.reply;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@RequiredArgsConstructor
+public class ReplyController {
+
+
+}
---
+++ src/main/java/dompoo/blog/reply/ReplyService.java
@@ -0,0 +1,13 @@
+package dompoo.blog.reply;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class ReplyService {
+
+    private final ReplyRepository repository;
+}
